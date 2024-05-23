b01705d40cf9d9eb20a22ae670a6889a2ee518c4
KeyaanKR <keyaan.edu@gmail.com>
2024-05-23 15:31:27 +0530

    created hittable, sphere classes

---
+++ src/sphere.h
@@ -0,0 +1,46 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+public:
+  sphere(const point_3 &center, double radius)
+      : center(center), radius(std::fmax(0, radius)){};
+  bool hit(const ray &r, double t_min, double t_max,
+           hit_record &rec) const override {
+    vector_3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = h * h - a * c;
+
+    if (discriminant < 0)
+      return false;
+
+    auto sqrtd = std::sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+    auto root = (h - sqrtd) / a;
+    if (root <= t_min || t_max <= root) {
+      root = (h + sqrtd) / a;
+      if (root <= t_min || t_max <= root) {
+        return false;
+      }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vector_3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+
+    return true;
+  }
+
+private:
+  point_3 center;
+  double radius;
+};
+
+#endif
